f563198a7e98372a4a9f3bacd9093e39d9c51d18
Danil Pleshakov <d.pleshakov@tools4brokers.com>
2016-03-09 19:33:45 +0300

    Change vertex type definition.

--- src/vertex_attrib.h
+++ src/vertex_attrib.h
@@ -4,36 +4,26 @@
 #include "typeid.h"
 #include "math3f.h"
 
-struct AttrPosition
+#define DEF_VERTEX_ATTR(name, type, count)\
+struct name: VertexAttr<count, type>\
+{name(type& v):VertexAttr(v){}}
+
+template<short count, class T>
+struct VertexAttr
 {
-    vec3f value;
-    static short elemCount(){ return 3; }
+    T value;
+    static short elemCount() { return count; };
     static int elemType(){ return TypeInfo<float>::GetId(); }
-    static size_t size(){ return sizeof(AttrPosition); }
+    static size_t size(){ return sizeof(T); }
+    
+protected:
+    VertexAttr(T _value)
+        : value(_value) {};
 };
 
-struct AttrNormal
-{
-    vec3f value;
-    static short elemCount(){ return 3; }
-    static int elemType(){ return TypeInfo<float>::GetId(); }
-    static size_t size(){ return sizeof(AttrNormal); }
-};
-
-struct AttrUV
-{
-    vec2f value;
-    static short elemCount(){ return 2; }
-    static int elemType(){ return TypeInfo<float>::GetId(); }
-    static size_t size(){ return sizeof(AttrUV); }
-};
-
-struct AttrRGBA
-{
-    vec4f value;
-    static short elemCount(){ return 4; }
-    static int elemType(){ return TypeInfo<float>::GetId(); }
-    static size_t size(){ return sizeof(AttrRGBA); }
-};
+DEF_VERTEX_ATTR(AttrPosition, vec3f, 3);
+DEF_VERTEX_ATTR(AttrNormal, vec3f, 3);
+DEF_VERTEX_ATTR(AttrUV, vec2f, 2);
+DEF_VERTEX_ATTR(AttrRGBA, vec4f, 4);
 
 #endif
