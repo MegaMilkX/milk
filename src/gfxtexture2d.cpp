a10521d28ea0456e4496c99b0683a1c7eaaad60b
MegaMilkX <gyrocopter3k@gmail.com>
2016-03-15 16:48:51 +0400

    Add glGenerateMipmap function

--- src/gfxtexture2d.cpp
+++ src/gfxtexture2d.cpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 
+#include "math3f.h"
+
 GFXTexture2D GFXTexture2D::Create()
 {
     GFXTexture2D texture;
@@ -12,18 +14,23 @@ GFXTexture2D GFXTexture2D::Create()
 GFXTexture2D GFXTexture2D::Create(File file)
 {
     GFXTexture2D texture = GFXTexture2D::Create();
-    float pixels[] = 
-    { 0.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 1.0f, 1.0f,
-      1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f, 1.0f };
-
+    //float pixels[] = 
+    //{ 0.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 1.0f, 1.0f,
+    //  1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f, 1.0f };
+    vec4f pixels[4096];
+    for(int i = 0; i < 4096; ++i)
+    {
+        pixels[i] = vec4f(rand()%1000 * 0.001f, rand()%1000 * 0.001f, rand()%1000 * 0.001f, 1.0f);
+    }
+    
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture.buffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_RGBA, GL_FLOAT, pixels);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    //glGenerateMipmap(GL_TEXTURE_2D);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 2, 2, 0, GL_RGBA, GL_FLOAT, pixels);
+    glGenerateMipmap(GL_TEXTURE_2D);
     
     return texture;
 }
