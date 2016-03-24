071c49d3978d23f9f146bd12100cc15cb15d082e
MegaMilkX <gyrocopter3k@gmail.com>
2016-03-24 22:13:11 +0300

    Add PushAttribute and GetAttribute definitions (unfinished)

--- src/r3d_new.h
+++ src/r3d_new.h
@@ -1,7 +1,7 @@
 #ifndef _R3D_H_
 #define _R3D_H_
 
-#include <vector>
+#include <set>
 
 class R3DAttrib
 {
@@ -16,6 +16,7 @@ class R3DPosition : public R3DAttrib
 class R3DData
 {
 public:
+    typedef int type_indexl
 	bool Read(File file);
 	bool Write(File file);
 	
@@ -24,7 +25,29 @@ public:
 	template<typename T>
 	T GetAttribute(int id = 0);
 private:
-	std::vector<R3DAttrib*> attributes;
+	std::map<type_index, std::vector<R3DAttrib*>> attributes;
 };
+
+template<typename T>
+void R3DData::PushAttribute(R3DAttr* data)
+{
+    R3DAttr* new_attr = data.clone();
+    attributes.insert(std::make_pair(new_attr->type_index(), new_attr));
+}
+
+template<typename T>
+T* R3DData::GetAttribute(int id = 0)
+{
+    std::vector<R3DAttrib*> vec = attributes[T::type_index()];
+    
+    if(id < vec.size() && id >= 0)
+    {
+        return (T*)vec[id];
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 #endif
