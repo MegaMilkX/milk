1ce005591f7ab5df980eeaf75cd7b81ad9b7f5a5
MegaMilkX <gyrocopter3k@gmail.com>
2016-03-26 23:05:33 +0300

    Add reworked R3DData class

--- src/util/r3dattrib.h
+++ src/util/r3dattrib.h
@@ -1,6 +1,8 @@
 #ifndef _R3DATTRIB_H_
 #define _R3DATTRIB_H_
 
+#include <vector>
+
 #define R3D_POSITION 100
 #define R3D_NORMAL 200
 #define R3D_TANGENT 300
@@ -14,21 +16,21 @@
 class IR3DAttrib
 {
 public:
-    virtual ~R3DAttrib(){}
+    virtual ~IR3DAttrib(){}
 };
 
 template<int TYPE_INDEX, typename ELEM_TYPE, int ELEM_COUNT>
-class R3DAttrib : public IR3DAttrib
+class R3DAttrib : public IR3DAttrib, public std::vector<ELEM_TYPE>
 {
 public:
-    static int type_index(){ return TYPE_INDEX; }
-    ELEM_TYPE operator[](int index)
+    R3DAttrib& operator<<(const ELEM_TYPE value)
     {
-        return value[index];
+        push_back(value);
+        return (*this);
     }
+    static int type_index(){ return TYPE_INDEX; }
 private:
-    ELEM_TYPE value[ELEM_COUNT];
-}
+};
 
 class R3DPosition : public R3DAttrib<R3D_POSITION, float, 3>
 {
--- src/util/r3ddata.h
+++ src/util/r3ddata.h
@@ -1,123 +1,78 @@
 #ifndef _R3DDATA_H_
 #define _R3DDATA_H_
 
+#include <map>
 #include <vector>
-#include <string>
-
-#include <stdint.h>
 
 #include "..\filesystem\file.h"
 
-#define R3D_POSITION 100
-#define R3D_NORMAL 200
-#define R3D_TANGENT 300
-#define R3D_BITANGENT 400
-#define R3D_UV 500
-#define R3D_COLOR 600
-#define R3D_BONE 700
-#define R3D_WEIGHT 800
-#define R3D_INDEX 900
-
-#define R3D_UNSIGNED_BYTE 1
-#define R3D_BYTE 2
-#define R3D_UNSIGNED_SHORT 3
-#define R3D_SHORT 4
-#define R3D_UNSIGNED_INT 5
-#define R3D_INT 6
-#define R3D_FLOAT 7
-#define R3D_DOUBLE 8
+#include "r3dattrib.h"
 
 class R3DData
 {
 public:
-    ~R3DData();
-    struct Attrib
-    {
-        int type;
-        int elemCount;
-        int dataType;
-        int count;
-        int size;
-        unsigned char* data;
-    };
-    
-    static R3DData Read(File file);
-    int AttribCount();
-    Attrib* GetAttrib(int i);
-    
+    typedef int type_index;
+    //bool Read(File file);
+    //bool Write(File file);
+
     template<typename T>
-    void AddAttrib(const std::vector<T>& data, int type, int elemPerVertex);
-    void Write(File file);
+    void PushAttribute(T& data);
+    template<typename T>
+    T* GetAttribute(unsigned int instance = 0);
+    template<typename T>
+    unsigned int AttribCount();
 private:
-    unsigned int r3d_next_id()
-    {
-        static unsigned int type_id = 0;
-        return ++type_id;
-    }
-
-    template<typename T>
-    unsigned int r3d_get_type_id()
-    {
-        static unsigned int id = r3d_next_id();
-        return id;
-    }
-
-    template<typename T>
-    unsigned int r3d_get_type_def()
-    {
-        unsigned int id = r3d_get_type_id<T>();
-        if (id == r3d_get_type_id<unsigned char>())
-            return R3D_UNSIGNED_BYTE;
-        else if (id == r3d_get_type_id<char>())
-            return R3D_BYTE;
-        else if (id == r3d_get_type_id<uint16_t>())
-            return R3D_UNSIGNED_SHORT;
-        else if (id == r3d_get_type_id<int16_t>())
-            return R3D_SHORT;
-        else if (id == r3d_get_type_id<uint32_t>())
-            return R3D_UNSIGNED_INT;
-        else if (id == r3d_get_type_id<int32_t>())
-            return R3D_INT;
-        else if (id == r3d_get_type_id<float_t>())
-            return R3D_FLOAT;
-        else if (id == r3d_get_type_id<double_t>())
-            return R3D_DOUBLE;
-        else
-            return 0;
-    }
-    
-    static int r3d_data_type_size(unsigned int type)
-    {
-        if(type == R3D_UNSIGNED_BYTE || type == R3D_BYTE)
-            return 1;
-        else if(type == R3D_UNSIGNED_SHORT || type == R3D_SHORT)
-            return 2;
-        else if(type == R3D_UNSIGNED_INT || type == R3D_INT
-                || type == R3D_FLOAT)
-            return 4;
-        else if(type == R3D_DOUBLE)
-            return 8;
-        
-        return 0;
-    }
-    
-    std::vector<Attrib> attribs;
+    std::map<type_index, std::vector<IR3DAttrib*>> attributes;
 };
 
 template<typename T>
-void R3DData::AddAttrib(const std::vector<T>& data, int type, int elemPerVertex)
+void R3DData::PushAttribute(T& data)
 {
-    Attrib attrib;
-    size_t buffer_len = data.size() * sizeof(T);
-    attrib.data = new unsigned char[buffer_len];
-    memcpy((void*)attrib.data, (void*)data.data(), buffer_len);
-    attrib.type = type;
-    attrib.elemCount = elemPerVertex;
-    attrib.dataType = r3d_get_type_def<T>();
-    attrib.count = data.size() / elemPerVertex;
-    attrib.size = buffer_len;
-    
-    attribs.push_back(attrib);
+    T* new_attr = new T();
+    memcpy(new_attr, &data, sizeof(T));
+
+    if (attributes.find(T::type_index()) == attributes.end())
+    {
+        std::vector<IR3DAttrib*> atrs;
+        atrs.push_back(new_attr);
+        attributes.insert(std::make_pair(T::type_index(), atrs));
+    }
+    else
+    {
+        std::map<type_index, std::vector<IR3DAttrib*>>::iterator it;
+        it = attributes.find(T::type_index());
+        it->second.push_back(new_attr);
+    }
+}
+
+template<typename T>
+T* R3DData::GetAttribute(unsigned int instance)
+{
+    std::vector<IR3DAttrib*> vec = attributes[T::type_index()];
+
+    if (instance < vec.size())
+    {
+        return (T*)vec[instance];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+template<typename T>
+unsigned int R3DData::AttribCount()
+{
+    if (attributes.find(T::type_index()) == attributes.end())
+    {
+        return 0;
+    }
+    else
+    {
+        std::map<type_index, std::vector<IR3DAttrib*>>::iterator it;
+        it = attributes.find(T::type_index());
+        return it->second.size();
+    }
 }
 
 #endif
