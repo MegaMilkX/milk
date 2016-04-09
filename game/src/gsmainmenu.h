351cce72d61f32c374f6710f54d3a5fdc59619e5
MegaMilkX <gyrocopter3k@gmail.com>
2016-04-09 23:39:23 +0300

    Change GSMainMenu

--- game/src/gsmainmenu.h
+++ game/src/gsmainmenu.h
@@ -10,13 +10,17 @@ public:
     {
         
     }
+    void OnSwitch()
+    {
+        glClearColor(0.8f, 0.4f, 0.2f, 1.0f);
+    }
     void OnUpdate()
     {
-        std::cout << "MAIN_MENU" << std::endl;
+        //std::cout << "MAIN_MENU" << std::endl;
     }
     void OnRender()
     {
-
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
     void OnCleanup()
     {
